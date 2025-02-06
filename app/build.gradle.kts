ebd366300e5d63d6005f9d9d4c33fef3fa768502
phuongne112 <114864268+phuongne112@users.noreply.github.com>
2025-02-06 20:59:50 +0700

    test lan 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.group5_onlinetourbookingsystem"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.group5_onlinetourbookingsystem"
